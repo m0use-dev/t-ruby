64289841e36545a6edb2a8a9b2d8a7e09460d7c8
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2022-12-10 08:52:35 +0000

    基本操作追加

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# t-ruby
+# Rubyテスト
+## 環境構築
 
-```
+###　バージョン確認
+```sh
 ruby -v
 ```
 
-
-```
+### 実行
+```sh
 ruby ファイル名
 ruby index.rb 
 ```
+
+## 参考サイト
+### 公式サイト
+https://www.ruby-lang.org/ja/
--- index.rb
+++ index.rb
@@ -1,9 +1,33 @@
-# 
+# 出力
 puts "出力テスト"
 
 
-# 
+# 変数
 num = 0
 puts num
 
 puts num.class
+
+#配列
+name = Array.new(["山田", "佐々木", "田中"] )
+puts name[0]
+
+# 条件分岐
+if num > 5
+  puts "大きい"
+elsif num ==5
+  puts "同じ"
+else
+  puts "小さい"
+end
+
+#　繰り返し
+for i in 0 .. 10
+  if i == 3
+    next #繰り返しを飛ばす
+  end
+  if i == 5
+    break #繰り返しを抜ける
+  end
+  puts i
+end
